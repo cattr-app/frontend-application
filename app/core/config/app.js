2baca0f454b203b18bca429d02869ea270d7b899
amazingcat LLC <team@amazingcat.net>
2020-03-20 21:21:33 +0700

    Add env file for Docker and null variant for API_URL at env

--- app/core/config/app.js
+++ app/core/config/app.js
@@ -3,7 +3,7 @@ import env from '_app/etc/env';
 
 console.log(env);
 
-axios.defaults.baseURL = (env.API_URL || 'http://localhost:8000') + '/' + (env.API_VERSION || '');
+axios.defaults.baseURL = (env.API_URL || `${window.location.origin}/api`) + '/' + (env.API_VERSION || '');
 axios.defaults.headers.common['X-REQUESTED-WITH'] = 'XMLHttpRequest';
 
 const token = localStorage.getItem('access_token');
