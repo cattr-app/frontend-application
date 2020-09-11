e3264d1ec4f6035f89fd3f7f20e72966092f93a7
Daniyar Adilov <daniyar@amazingcat.net>
2020-09-11 10:51:29 +0200

    Resolve "Dynamic dropdown menu"

--- app/core/router/index.js
+++ app/core/router/index.js
@@ -7,7 +7,6 @@ const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
     return originalPush.call(this, location).catch(err => err);
 };
-//
 
 Vue.use(VueRouter);
 
