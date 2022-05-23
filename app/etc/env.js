faf333dae273d8d941f5b1e00b7f0d487561f753
Alex Vyarvelsky <alex@cattr.app>
2022-05-24 00:27:00 +0700

    fix: default env values

--- app/etc/env.js
+++ app/etc/env.js
@@ -1,6 +1,6 @@
 module.exports = {
     // If set, frontend will request backend at this url; location origin otherwise
-    API_URL: 'http://127.0.0.1:8000',
+    API_URL: null,
 
     // Will be used at the about page
     STORAGE_SPACE_MAX_USED: 75,
