963c8be6eb85563eba804edce1e06a296ab19bf4
Danil Shapilov <danils@amazingcat.net>
2022-09-10 13:23:15 +0500

    revert: "fix: screenshots displays on true page; task done"
    This reverts commit 748fc4daf820260f90c6677c6609f6c5c4c14c7a.

--- app/core/components/Screenshot.vue
+++ app/core/components/Screenshot.vue
@@ -135,7 +135,7 @@
                 if (!timezone || !this.interval.start_at) {
                     return;
                 } else if (timezone === this.companyData['timezone']) {
-                    return moment.tz(this.interval.start_at, 'gmt').format('HH:mm');
+                    return moment(this.interval.start_at).format('HH:mm');
                 }
                 return moment.tz(this.interval.start_at, this.companyData['timezone']).tz(timezone).format('HH:mm');
             },
